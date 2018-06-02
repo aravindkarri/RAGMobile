9e8682afa0cd95a874d5216ea413158c503084c2
aravindkarri <39855404+aravindkarri@users.noreply.github.com>
2018-06-02 17:23:11 +0530

    Initial mobile App Running
    Initial mobile App Running

---
+++ app/src/main/java/UserData.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+
+public class UserData
+{
+
+    String frrstName;
+    String lastName;
+    String MiddleName;
+    int id;
+    String email;
+    Date dateofbirth;
+
+
+
+}
